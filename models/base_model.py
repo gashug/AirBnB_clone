09ea2e232aa11fe05fd03d3ca64c1bac179ae3bf
gashug <geoffrey.gachuiri98@gmail.com>
2023-05-13 13:48:05 -0700

    Updated the BaseModel class for instance re-creation

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,19 @@ class BaseModel:
     created_at: datetime
     updated_at: datetime
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize public instance attrubutes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """return string representation of object"""
